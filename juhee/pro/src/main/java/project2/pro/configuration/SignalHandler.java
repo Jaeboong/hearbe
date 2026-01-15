761d9a2a395f59bb3360072c75c0baf51b04c3d0
juhee <miy7625@naver.com>
2026-01-16 08:53:54 +0900

    1

---
+++ juhee/pro/src/main/java/project2/pro/configuration/SignalHandler.java
@@ -0,0 +1,31 @@
+package project2.pro.configuration;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.List;
+
+public class SignalHandler extends TextWebSocketHandler {
+    private static final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        // 나를 제외한 모든 세션에 메시지 전달
+        for (WebSocketSession webSocketSession : sessions) {
+            if (webSocketSession.isOpen() && !webSocketSession.getId().equals(session.getId())) {
+                webSocketSession.sendMessage(message);
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, org.springframework.web.socket.CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+}
