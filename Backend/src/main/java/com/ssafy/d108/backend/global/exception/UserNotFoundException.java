df6e3d7f83cdd9ef096314adfc5097a66d032062
황수빈 <tnqls7043@naver.com>
2026-01-23 13:13:45 +0900

    feat: Exception 파일 생성

---
+++ Backend/src/main/java/com/ssafy/d108/backend/global/exception/UserNotFoundException.java
@@ -0,0 +1,21 @@
+package com.ssafy.d108.backend.global.exception;
+
+import com.ssafy.d108.backend.global.response.ErrorCode;
+
+/**
+ * 사용자를 찾을 수 없을 때 발생하는 예외
+ */
+public class UserNotFoundException extends BusinessException {
+
+    public UserNotFoundException() {
+        super(ErrorCode.USER_NOT_FOUND);
+    }
+
+    public UserNotFoundException(Long userId) {
+        super(ErrorCode.USER_NOT_FOUND, "사용자를 찾을 수 없습니다. ID: " + userId);
+    }
+
+    public UserNotFoundException(String loginId) {
+        super(ErrorCode.USER_NOT_FOUND, "사용자를 찾을 수 없습니다. 로그인 ID: " + loginId);
+    }
+}
