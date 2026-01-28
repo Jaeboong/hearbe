be516d622c4a01de552e3782603a8047483d9b7a
juhee <miy7625@naver.com>
2026-01-28 15:34:05 +0900

    feat: SecurityUtil 인증 로직 활성화

--- Backend/src/main/java/com/ssafy/d108/backend/global/config/SecurityConfig.java
+++ Backend/src/main/java/com/ssafy/d108/backend/global/config/SecurityConfig.java
@@ -50,7 +50,9 @@ public class SecurityConfig {
                                 "/api/auth/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-resources/**")
+                                "/swagger-resources/**",
+                                "/cart/**",
+                                "/wishlist/**")
                         .permitAll()
 
                         // All other requests require authentication
